a78821e442323a3490db5371a2c05772cbc8ce93
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-02-18 08:10:16 -0700

    Started Shuffle The Deck Assignment

---
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -0,0 +1,14 @@
+﻿Option Strict On
+Option Explicit On
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Shuffle The Deck
+'https://github.com/AftaAnfi/ShuffleTheDeck.git
+Module ShuffleTheDeck
+
+    Sub Main()
+
+    End Sub
+
+End Module
